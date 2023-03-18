749f869085346ec4586efca3ddcfd677088e8f62
Maksim Zheleznov <zheleznov.maxim@gmail.com>
2023-03-18 16:24:49 +0300

    Added .env example // Move secret out of code

--- src/config/settings.py
+++ src/config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hhyjd^a-73-v#ca5^4h4+k#(48ont$2)u5i!m%=0j#mo@s%hv5'
+SECRET_KEY = os.getenv('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
